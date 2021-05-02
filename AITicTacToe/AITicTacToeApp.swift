03ef077f8d91d1208fb329f3771a3f11a98103df
Petros Tepoyan <petros@tepoyan.ru>
2021-05-03 00:55:13 +0300

    Initial Commit

---
+++ AITicTacToe/AITicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITicTacToeApp.swift
+//  AITicTacToe
+//
+//  Created by Петрос Тепоян on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AITicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
